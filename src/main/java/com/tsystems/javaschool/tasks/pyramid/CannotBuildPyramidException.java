1adacbe0f91b0f8d0ad56dc7a692c93165bff5fc
y-tak <y-tak@yandex.ru>
2019-09-02 14:24:45 +0300

    ++

--- src/main/java/com/tsystems/javaschool/tasks/pyramid/CannotBuildPyramidException.java
+++ src/main/java/com/tsystems/javaschool/tasks/pyramid/CannotBuildPyramidException.java
@@ -1,4 +1,12 @@
 package com.tsystems.javaschool.tasks.pyramid;
 
 public class CannotBuildPyramidException extends RuntimeException {
+
+    public CannotBuildPyramidException(String message) {
+        super(message);
+    }
+
+    public CannotBuildPyramidException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
